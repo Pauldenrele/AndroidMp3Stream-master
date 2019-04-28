f57ad0f2738a90e16980bd5ecbe3dbeff0823f4a
PAULI <adenreleademola@yahoo.com>
2019-04-28 20:15:32 +0100

    Initial commit

---
+++ app/src/main/java/edmt/dev/androidmp3stream/Main2Activity.java
@@ -0,0 +1,246 @@
+package edmt.dev.androidmp3stream;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+    private ProgressDialog mDialog;
+    String AudioSavePathInDevice = null;
+    MediaRecorder mediaRecorder;
+    Random random;
+    MediaPlayer mediaPlayer;
+    String RandomAudioFileName = "ABCDEFGHIJKLMNOP";
+    public static final int RequestPermissionCode = 1;
+    private ImageButton micButton;
+    private StorageReference mStorage;
+    public static final String TAG = Main2Activity.class.getSimpleName();
+    private File mediaStorageDir;
+    private Button mStart, mStop;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        random = new Random();
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+
+        mDialog = new ProgressDialog(this);
+
+
+        mediaStorageDir = new File(Environment.getExternalStorageDirectory(), "Recorder");
+
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("App", "failed to create directory");
+            }
+        }
+
+        mStart = findViewById(R.id.start_button);
+        mStop = findViewById(R.id.stop_button);
+        mStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAudio();
+                Toast.makeText(getApplicationContext(),"recording",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopAudio();
+            }
+        });
+
+
+
+        //micButton = findViewById(R.id.mic);
+
+//
+//        micButton.setOnTouchListener(new View.OnTouchListener() {
+//
+//            @Override
+//
+//            public boolean onTouch(View v, MotionEvent event) {
+//                if (checkPermission()) {
+//                    int count = 0;
+//                    AudioSavePathInDevice = mediaStorageDir.getAbsolutePath() + "/" + "AudioRecording" + count + ".3gp";
+//                    Log.d(TAG, "on touch listener" + AudioSavePathInDevice);
+//                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+//                        Toast.makeText(Main2Activity.this, "recording", Toast.LENGTH_SHORT).show();
+//
+//                        MediaRecorderReady();
+//                        try {
+//
+//                            Toast.makeText(Main2Activity.this, AudioSavePathInDevice, Toast.LENGTH_SHORT).show();
+//                            mediaRecorder.prepare();
+//                            mediaRecorder.start();
+//                        } catch (IllegalStateException e) {
+//
+//                            e.printStackTrace();
+//                        } catch (IOException e) {
+//
+//                            e.printStackTrace();
+//                        }
+//
+//                    } else if (event.getAction() == MotionEvent.ACTION_UP) {
+//                        mediaRecorder.stop();
+//                        Toast.makeText(getApplicationContext(), "recording Stopped", Toast.LENGTH_SHORT).show();
+//                        mediaPlayer = new MediaPlayer();
+//
+//                        try {
+//                            mediaPlayer.setDataSource(AudioSavePathInDevice);
+//                            // uploadAudio();
+//                            mediaPlayer.prepare();
+//                        } catch (IOException e) {
+//                            e.printStackTrace();
+//                        }
+//
+//
+//                        mediaPlayer.start();
+//                        uploadAudio();
+//                        Toast.makeText(Main2Activity.this, "Recording Playing",
+//                                Toast.LENGTH_LONG).show();
+//                        Toast.makeText(Main2Activity.this, AudioSavePathInDevice, Toast.LENGTH_SHORT).show();
+//
+//
+//                    }
+//                } else {
+//                    requestPermission();
+//                }
+//
+//                return true;
+//            }
+//        });
+    }
+
+
+    public void MediaRecorderReady() {
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        mediaRecorder.setOutputFile(AudioSavePathInDevice);
+    }
+
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(Main2Activity.this, new
+                String[]{WRITE_EXTERNAL_STORAGE, RECORD_AUDIO}, RequestPermissionCode);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case RequestPermissionCode:
+                if (grantResults.length > 0) {
+                    boolean StoragePermission = grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED;
+
+                    boolean RecordPermission = grantResults[1] ==
+                            PackageManager.PERMISSION_GRANTED;
+
+                    if (StoragePermission && RecordPermission) {
+                        Toast.makeText(Main2Activity.this, "Permission Granted",
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(Main2Activity.this, "Permission Denied", Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
+        }
+    }
+
+    public boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(),
+                WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(),
+                RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED &&
+                result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void uploadAudio() {
+        Toast.makeText(getApplicationContext(), "Uploading", Toast.LENGTH_SHORT).show();
+        mDialog.setMessage("uploading audio");
+        mDialog.show();
+
+        StorageReference filename = mStorage.child("Audio").child("new_audio.3gp");
+        Uri uri = Uri.fromFile(new File(AudioSavePathInDevice));
+        Log.d(TAG, "Upload method  " + String.valueOf(uri.toString()));
+
+        filename.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                mDialog.dismiss();
+
+            }
+        });
+    }
+
+
+    private void startAudio() {
+        if (checkPermission()) {
+            int count = 0;
+            AudioSavePathInDevice = mediaStorageDir.getAbsolutePath() + "/" + "AudioRecording" + count + ".3gp";
+            Toast.makeText(Main2Activity.this, "recording", Toast.LENGTH_SHORT).show();
+
+            MediaRecorderReady();
+            try {
+
+                Toast.makeText(Main2Activity.this, AudioSavePathInDevice, Toast.LENGTH_SHORT).show();
+                mediaRecorder.prepare();
+                mediaRecorder.start();
+            } catch (IllegalStateException e) {
+
+                e.printStackTrace();
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+
+        } else {
+            requestPermission();
+        }
+
+    }
+
+    private void stopAudio() {
+        if (mediaRecorder != null) {
+            mediaRecorder.stop();
+            uploadAudio();
+        }
+
+    }
+}
---
+++ app/src/main/java/edmt/dev/androidmp3stream/MainActivity.java
@@ -0,0 +1,166 @@
+package edmt.dev.androidmp3stream;
+
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+import dyanamitechetan.vusikview.VusikView;
+
+public class MainActivity extends AppCompatActivity implements MediaPlayer.OnBufferingUpdateListener,MediaPlayer.OnCompletionListener{
+
+    private ImageButton btn_play_pause;
+    private SeekBar seekBar;
+    private TextView textView;
+   private Button nxtBtn;
+    private VusikView musicView;
+
+    private MediaPlayer mediaPlayer;
+    private int mediaFileLength;
+    private int realtimeLength;
+    final Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       nxtBtn = findViewById(R.id.nxt_act);
+        musicView = (VusikView)findViewById(R.id.musicView);
+
+
+    nxtBtn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this , Main2Activity.class);
+            startActivity(intent);
+        }
+    });
+        seekBar = (SeekBar)findViewById(R.id.seekbar);
+        seekBar.setMax(99); // 100% (0~99)
+        seekBar.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(mediaPlayer.isPlaying())
+                {
+                    SeekBar seekBar = (SeekBar)v;
+                    int playPosition = (mediaFileLength/100)*seekBar.getProgress();
+                    mediaPlayer.seekTo(playPosition);
+                }
+                return false;
+            }
+        });
+
+        textView = (TextView)findViewById(R.id.textTimer);
+
+        btn_play_pause = (ImageButton) findViewById(R.id.btn_play_pause);
+        btn_play_pause.setOnClickListener(new View.OnClickListener() {
+
+
+
+
+            @Override
+            public void onClick(View v) {
+                final ProgressDialog mDialog = new ProgressDialog(MainActivity.this);
+
+
+                AsyncTask<String,String,String> mp3Play = new AsyncTask<String, String, String>() {
+
+                    @Override
+                    protected void onPreExecute() {
+                        mDialog.setMessage("Please wait");
+                        mDialog.show();
+                    }
+
+                    @Override
+                    protected String doInBackground(String... params) {
+                        try{
+                            mediaPlayer.setDataSource(params[0]);
+                            mediaPlayer.prepare();
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+                        return "";
+                    }
+
+                    @Override
+                    protected void onPostExecute(String s) {
+                        mediaFileLength = mediaPlayer.getDuration();
+                        realtimeLength = mediaFileLength;
+                        if(!mediaPlayer.isPlaying())
+                        {
+                            mediaPlayer.start();
+                            btn_play_pause.setImageResource(R.drawable.ic_pause);
+                        }
+                        else
+                        {
+                            mediaPlayer.pause();
+                            btn_play_pause.setImageResource(R.drawable.ic_play);
+                        }
+
+                        updateSeekBar();
+                        mDialog.dismiss();
+                    }
+                };
+
+                mp3Play.execute("https://firebasestorage.googleapis.com/v0/b/audio-f6e2b.appspot.com/o/Audio%2Fnew_audio.3gp?alt=media&token=a1cb3fd5-afe0-4660-82f5-1a297551a1f4"); // direct link mp3 file
+
+                musicView.start();
+            }
+        });
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setOnBufferingUpdateListener(this);
+        mediaPlayer.setOnCompletionListener(this);
+
+
+    }
+
+    private void updateSeekBar() {
+        seekBar.setProgress((int)(((float)mediaPlayer.getCurrentPosition() / mediaFileLength)*100));
+        if(mediaPlayer.isPlaying())
+        {
+            Runnable updater = new Runnable() {
+                @Override
+                public void run() {
+                    updateSeekBar();
+                    realtimeLength-=1000; // declare 1 second
+                    textView.setText(String.format("%d:%d", TimeUnit.MILLISECONDS.toMinutes(realtimeLength),
+                            TimeUnit.MILLISECONDS.toSeconds(realtimeLength) -
+                                    TimeUnit.MILLISECONDS.toSeconds(TimeUnit.MILLISECONDS.toMinutes(realtimeLength))));
+
+                }
+
+            };
+            handler.postDelayed(updater,1000); // 1 second
+        }
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+        seekBar.setSecondaryProgress(percent);
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        btn_play_pause.setImageResource(R.drawable.ic_play);
+        musicView.stopNotesFall();
+
+    }
+}
+
+
+//mp3Play.execute("https://firebasestorage.googleapis.com/v0/b/audio-f6e2b.appspot.com/o/Audio%2Fnew_audio.3gp?alt=media&token=a1cb3fd5-afe0-4660-82f5-1a297551a1f4"); // direct link mp3 file
